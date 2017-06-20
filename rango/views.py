e9052dcdb6cce41fc6ef27562faf8aea8e8872c5
NathanDawson <s161675@greenhead.ac.uk>
2017-06-20 14:59:48 +0100

    Sixth commit, added a form that is able  to add categories

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
 
 def index(request):
     category_list=  Category.objects.order_by('-likes')[:5]
@@ -29,3 +30,16 @@ def category(request, category_name_slug):
         pass
 
     return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print (form.errors)
+    else:
+        form=CategoryForm()
+
+    return render(request, 'rango/add_category.html', {'form': form})
