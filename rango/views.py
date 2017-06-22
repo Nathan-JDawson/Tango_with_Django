4e9b9354d197e2a1de6499c4769c6ab7dea47239
NathanDawson <s161675@greenhead.ac.uk>
2017-06-22 13:18:52 +0100

    Ninth Commit. Added logout link, as well as tested restricted access.

--- rango/views.py
+++ rango/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 from rango.models import Category
 from rango.models import Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     category_list=  Category.objects.order_by('-likes')[:5]
@@ -139,3 +140,12 @@ def user_login(request):
         # No context variables to pass to the template system, hence the
         # blank dictionary object...
         return render(request, 'rango/login.html', {})
+
+@login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text!")
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/rango/')
--- tango_with_django/settings.py
+++ tango_with_django/settings.py
@@ -90,3 +90,5 @@ STATICFILES_DIRS = (STATIC_PATH,)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_URL='/rango/login'
