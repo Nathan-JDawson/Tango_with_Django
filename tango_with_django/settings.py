5287fac219cf10aee69f88264cbadca956123547
NathanDawson <s161675@greenhead.ac.uk>
2017-06-23 12:13:07 +0100

    Twelfth commit. My sophisticated login functionality. Started using bootstrap for templates.

--- tango_with_django/settings.py
+++ tango_with_django/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -91,4 +92,10 @@ STATICFILES_DIRS = (STATIC_PATH,)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-LOGIN_URL='/rango/login'
+LOGIN_URL='/accounts/login/'
+
+REGISTRATION_OPEN = True
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_AUTO_LOGIN = True
+LOGIN_REDIRECT_URL = '/rango/'
+LOGIN_URL = '/accounts/login/'
